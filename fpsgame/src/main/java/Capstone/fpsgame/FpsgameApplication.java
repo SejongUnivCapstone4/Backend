b02820ee157b9ad3e19c4800c08fcc818b4b7f4d
Munhangyeol <mhg10181018@gmail.com>
2024-10-04 14:49:24 +0900

    initial

---
+++ fpsgame/src/main/java/Capstone/fpsgame/FpsgameApplication.java
@@ -0,0 +1,13 @@
+package Capstone.fpsgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpsgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FpsgameApplication.class, args);
+	}
+
+}
---
+++ fpsgame/src/test/java/Capstone/fpsgame/FpsgameApplicationTests.java
@@ -0,0 +1,13 @@
+package Capstone.fpsgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpsgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
